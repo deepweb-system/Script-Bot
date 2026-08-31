8042acc20bf579e7ab67df4060e55c46763312d8
AHSAN HABIB MUAZ <68775472+ahsanhabibmuaz@users.noreply.github.com>
2026-08-31 07:09:11 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+module.exports = {
+  botName: 'Special Script Bot',
+  ownerNumber: '966500896152',
+  footer: '\n\n> ⓘSpecial Script by *Muaz*.',
+  antideleteEnabled: true,
+  viewOnceForwardEnabled: true,
+  autoStatusCapture: true,
+  maxStoredMessages: 500,
+  mediaMaxAgeMs: 24 * 60 * 60 * 1000,
+  maxSongFileBytes: 50 * 1024 * 1024
+};
